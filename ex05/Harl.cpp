10faff3a27c4d41dd966f471c31a836c0b0e4a2c
Yap Jien Fei <yapjienfei1998@gmail.com>
2026-05-29 12:44:52 +0800

    feat: Complete ex05 and fix Makefile for ex04

---
+++ ex05/Harl.cpp
@@ -0,0 +1,41 @@
+#include "Harl.hpp"
+#include <iostream>
+
+void Harl::debug(void)
+{
+    std::cout << "[DEBUG] I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info(void)
+{
+    std::cout << "[INFO] I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void Harl::warning(void)
+{
+    std::cout << "[WARNING] I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month." << std::endl;
+}
+
+void Harl::error(void)
+{
+    std::cout << "[ERROR] This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+//void (Harl::*ptr)(void);
+//void return, and void parameter
+// ptr is pointer to a member of class Harl
+// ptr = &Harl::debug;
+void Harl::complain(std::string level)
+{
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    void (Harl::*functions[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    for (int i = 0; i < 4; i++)
+    {
+        if (level == levels[i])
+        {
+            (this->*functions[i])();
+            return;
+        }
+    }
+    std::cout << "[UNKNOWN] Did not match any log levels." << std::endl;
+}
